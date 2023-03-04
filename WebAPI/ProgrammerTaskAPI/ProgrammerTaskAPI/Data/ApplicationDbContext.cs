0733979617d58300d17a3a34754183abc66925f5
Nazar Plahtiy <nazarplahtij@gmail.com>
2023-03-04 17:31:26 +0200

    MainApi

---
+++ WebAPI/ProgrammerTaskAPI/ProgrammerTaskAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammerTaskAPI.Models;
+
+namespace ProgrammerTaskAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<WorkTask> Tasks { get; set; }
+    }
+}
